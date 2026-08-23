3953841e790e6e49248eb8d2b1ab637491f45b6b
safeofficial <safe.official.sogang@gmail.com>
2026-08-23 23:14:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SAFE_CONFIG = {
   SUPABASE_URL:      'https://vxccyrwnrewsnbmgqiqd.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_YPRU6IL_gV9hsdYwD1CkPw_wUDuX0_2'
+  SUPABASE_ANON_KEY: 'sb_publishable_YPRU6IL_gV9hsdYwD1CkPw_wUDuX0_2',
   LOGIN_DOMAIN:      '@safe.sogang'
 };
