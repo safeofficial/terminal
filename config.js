96cc6aefa72181628f348bd1ba152543743f3fa5
safeofficial <safe.official.sogang@gmail.com>
2026-08-23 22:58:44 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SAFE_CONFIG = {
-  SUPABASE_URL:      'https://여기에-프로젝트-ID.supabase.co',
-  SUPABASE_ANON_KEY: '여기에-publishable-key',
+  SUPABASE_URL:      'https://vxccyrwnrewsnbmgqiqd.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_YPRU6IL_gV9hsdYwD1CkPw_wUDuX0_2'
   LOGIN_DOMAIN:      '@safe.sogang'
 };
